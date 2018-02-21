1907690fee906f6649c14c0c4d762b05814c49f8
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2018-02-20 22:22:28 -0800

    handle connection in multiple threads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("localhost:9000").unwrap();
@@ -8,7 +9,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
